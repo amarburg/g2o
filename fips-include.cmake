d21d6a5996210fd5811ca299aa34afa64fe6535b
Aaron Marburg <amarburg@apl.washington.edu>
2017-11-22 13:26:06 -0800

    Added g2o/types/sba and g2o/types/slam3d directories.

--- fips-include.cmake
+++ fips-include.cmake
@@ -1,2 +1,8 @@
 ## Allows g2o/config.h to be found
 fips_include_directories( ${FIPS_PROJECT_BUILD_DIR}/include/ )
+
+set( G2O_LIBRARIES g2o_core
+              g2o_types_data
+              g2o_types_slam2d
+              g2o_solver_csparse
+              g2o_csparse_extension )
