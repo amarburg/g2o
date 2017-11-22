256cb0162eb612c9b46e08178865d84190f8e0b2
Aaron Marburg <amarburg@apl.washington.edu>
2017-11-22 14:19:32 -0800

    Set FIPS_RTTI in fips-include.cmake

--- fips-include.cmake
+++ fips-include.cmake
@@ -1,8 +1,14 @@
+## This is required for g2o
+message( STATUS   "fips-g2o requires RTTI, enabling....")
+set(FIPS_RTTI on CACHE BOOL  "" FORCE)
+
 ## Allows g2o/config.h to be found
 fips_include_directories( ${FIPS_PROJECT_BUILD_DIR}/include/ )
 
 set( G2O_LIBRARIES g2o_core
               g2o_types_data
               g2o_types_slam2d
+              g2o_types_sba
+              g2o_types_slam3d
               g2o_solver_csparse
               g2o_csparse_extension )
